6079145c47697b26d26e81e6951bc4cf3a67e3e6
JOÃO PEDRO LIMEIRA RODRIGUES <joao.rodrigues26@portalsesisp.org.br>
2026-04-17 13:05:05 -0300

    Dashboard iniciado

--- src/app/shared/cartao/cartao.ts
+++ src/app/shared/cartao/cartao.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cartao',
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './cartao.html',
   styleUrl: './cartao.css',
 })
-export class Cartao {}
+export class Cartao {
+  @Input() icon: string = 'group';
+}
