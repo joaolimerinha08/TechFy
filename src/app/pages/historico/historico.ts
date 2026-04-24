02369e85b08ea474ae0b182880c819df989316e7
BENETT REZENDE <benett.rezende@portalsesisp.org.br>
2026-04-24 08:55:50 -0300

    Página de histórico estilizada

--- src/app/pages/historico/historico.ts
+++ src/app/pages/historico/historico.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-historico',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule],
   templateUrl: './historico.html',
   styleUrl: './historico.css',
 })
-export class Historico {}
+export class Historico {
+  // Lista de dados simplificada
+  saidas = [
+    { 
+      data: '13/03/2026', 
+      aluno: 'Ana Beatriz Gomes', 
+      turma: '5º Ano A', 
+      horario: '12:05', 
+      liberadoPara: 'Marta Gomes (Mãe)', 
+      local: 'Portaria 1' 
+    }
+  ];
+}
