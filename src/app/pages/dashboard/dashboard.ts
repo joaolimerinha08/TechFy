081359551c781d07e9959b5b500e09fe6e84bb94
JOÃO PEDRO LIMEIRA RODRIGUES <joao.rodrigues26@portalsesisp.org.br>
2026-04-24 09:16:07 -0300

    Finalizando dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -3,11 +3,12 @@ import { BotaoConfirmar } from "../../shared/botao-confirmar/botao-confirmar";
 import { BotaoCancelar } from "../../shared/botao-cancelar/botao-cancelar";
 import { Cartao } from "../../shared/cartao/cartao";
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink} from "@angular/router";
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [BotaoConfirmar, Cartao, MatIconModule],
+  imports: [BotaoConfirmar, Cartao, MatIconModule, BotaoCancelar, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
