1a6813a65961b23a07cd774a6630a1669abb9a96
ANTHONY ERICK SIQUEIRA DIAS <anthony.siqueira@portalsesisp.org.br>
2026-04-24 09:26:24 -0300

    Editado login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css'],
 })
 export class Login {}
